85c53db301686db47896ccc4a3d53e9d9ce7ba35
Aldo Nicolás Mellado Opazo <aldomellado.1310@gmail.com>
2023-11-18 23:16:51 -0300

    18_11-Entregado

--- app.py
+++ app.py
@@ -1,24 +1,78 @@
-# Importamos librerias necesarias para proyecto
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-# Ejecutamos la lectura de los archivos que nos permitirán ejecutar el análisis del proyecto
+# Carga de datos (asegúrate de que la ruta al archivo CSV sea la correcta)
+car_data = pd.read_csv('./notebooks/vehicles_us.csv')
 
-# Lectura de los archivos
+# Creación del título y descripción en la app web
 st.header("Web App - Data Visualization")
-car_data = pd.read_csv('./notebooks/vehicles_us.csv')  # leer los datos
-hist_button = st.button('Construir histograma')  # crear un botón
+st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+st.write('Seleccione alguna de las opciones que siguen:')
 
+# Creación de checkboxes para selección de histogramas
+check1 = st.checkbox("Crear histograma según 'paint_color'")
+check2 = st.checkbox("Crear histograma según 'condition'")
+check3 = st.checkbox("Crear histograma según 'type'")
+check4 = st.checkbox("Crear histograma según 'fuel")
 
-if hist_button:  # al hacer clic en el botón
-    # escribir un mensaje
-    st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+# Lista para almacenar los gráficos seleccionados
+selected_charts = []
 
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer", color="odometer")
-    # fig = px.scatter(car_data, x="odometer", color="odometer")
+# Creación de histogramas basados en las selecciones de los checkboxes
+if check1:
+    fig1 = px.histogram(car_data, x="paint_color", color="paint_color",
+                        title="Distribución de vehículos por color.")
+    selected_charts.append(fig1)
+if check2:
+    fig2 = px.histogram(car_data, x="condition", color="condition",
+                        title="Distribución de vehículos por estado.")
+    selected_charts.append(fig2)
+if check3:
+    fig3 = px.histogram(car_data, x="type", color="type",
+                        title="Distribución de vehículos por tipo.")
+    selected_charts.append(fig3)
+if check4:
+    fig4 = px.histogram(car_data, x="fuel", color="fuel",
+                        title="Distribución de vehículos por combustible utilizado.")
+    selected_charts.append(fig4)
 
-    # mostrar un gráfico Plotly interactivo
+# Mostrar los histogramas seleccionados
+for fig in selected_charts:
+    st.plotly_chart(fig, use_container_width=True)
+
+# Mensaje y gráficos adicionales
+st.header("Web App - Análisis adicional")
+
+# Menú desplegable para seleccionar el gráfico
+option = st.selectbox(
+    'Selecciona el gráfico que deseas visualizar:',
+    ('Distribución del Kilometraje', 'Kilometraje vs Precio', 'Distribución de días de publicación',
+     'Tiempo Medio de Publicación por tipo de vehículo y Condición')
+)
+
+# Visualización del gráfico seleccionado
+if option == 'Distribución del Kilometraje':
+    fig = px.histogram(car_data, x='odometer',
+                       color='odometer',
+                       title='Distribución del Kilometraje')
+    st.plotly_chart(fig, use_container_width=True)
+
+elif option == 'Kilometraje vs Precio':
+    fig = px.scatter(car_data, x='odometer', y='price',
+                     color='price',
+                     title='Kilometraje vs Precio')
+    st.plotly_chart(fig, use_container_width=True)
+
+elif option == 'Distribución de días de publicación':
+    fig = px.histogram(car_data, x='days_listed',
+                       color='days_listed',
+                       title='Distribución de días de publicación')
+    st.plotly_chart(fig, use_container_width=True)
+
+elif option == 'Tiempo Medio de Publicación por Modelo y Condición':
+    grouped_data = car_data.groupby(['model', 'condition'])[
+        'days_listed'].mean().reset_index()
+    fig = px.bar(grouped_data, x='model', y='days_listed', color='condition',
+                 barmode='group', title='Tiempo Medio de Publicación por Modelo y Condición')
     st.plotly_chart(fig, use_container_width=True)
